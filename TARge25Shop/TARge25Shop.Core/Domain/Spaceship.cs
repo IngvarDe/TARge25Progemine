daa52a3c99d7164d8c9053ca346842fb0937b6e4
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2026-09-07 17:44:02 +0300

    SapceshipCreate

--- TARge25Shop/TARge25Shop.Core/Domain/Spaceship.cs
+++ TARge25Shop/TARge25Shop.Core/Domain/Spaceship.cs
@@ -10,6 +10,5 @@
 
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
-
     }
 }
--- TARge25Shop/TARge25Shop.Data/TARge25ShopContext.cs
+++ TARge25Shop/TARge25Shop.Data/TARge25ShopContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TARge25Shop.Core.Domain;
 
 
 namespace TARge25Shop.Data
@@ -10,5 +11,8 @@ namespace TARge25Shop.Data
         public TARge25ShopContext(DbContextOptions<TARge25ShopContext> options)
             : base(options) { }
 
+
+        //vaja lisada dbSet, mis on seotud meie domain klassiga Spaceship
+        public DbSet<Spaceship> Spaceships { get; set; }
     }
 }
