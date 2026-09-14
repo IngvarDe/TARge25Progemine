5e181f127a921bffb4051b866140ab7856decfd2
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2026-09-14 17:41:50 +0300

    SpaceshipUpdatePost

--- TARge25Shop/TARge25Shop/Controllers/SpaceshipController.cs
+++ TARge25Shop/TARge25Shop/Controllers/SpaceshipController.cs
@@ -87,10 +87,42 @@ namespace TARge25Shop.Controllers
                 Name = spaceship.Name,
                 ShipType = spaceship.ShipType,
                 Crew = spaceship.Crew,
-                EnginePower = spaceship.EnginePower
+                EnginePower = spaceship.EnginePower,
+                CreatedAt = spaceship.CreatedAt,
+                UpdatedAt = spaceship.UpdatedAt
             };
 
             return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(SpaceshipUpdateViewModel vm)
+        {
+            var dto = new SpaceshipDto()
+            {
+                Id = vm.Id,
+                Name = vm.Name,
+                ShipType = vm.ShipType,
+                Crew = vm.Crew,
+                EnginePower = vm.EnginePower,
+                CreatedAt = vm.CreatedAt,
+                UpdatedAt = vm.UpdatedAt
+            };
+
+            var result = await _spaceshipServices.Update(dto);
+
+            if (result == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+
+            return View();
         }
     }
 }
